7732549749ad79542c11e44ca9cdbe79380d15af
Jason Hedlund <jhedlund@hawk.iit.edu>
2015-11-22 15:12:48 -0600

    Simplified and removed syntax errors

--- setup.php
+++ setup.php
@@ -3,6 +3,7 @@
 require 'vendor/autoload.php';
 
 use Aws\Rds\RdsClient;
+
 $client = RdsClient::factory(array(
 'region'  => 'us-east-1'
 'version' => 'latest'
@@ -14,57 +15,37 @@ $result = $client->describeDBInstances(array(
 ));
 
 
-$endpoint = ""; 
-
-
-foreach ($result->getPath('DBInstances/*/Endpoint/Address') as $ep) {
-    // Do something with the message
-    echo "============". $ep . "================";
-    $endpoint = $ep;
-}
-
-
+$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 
 echo "begin database";
 $link = mysqli_connect($endpoint,"jhedlund","letmeinplease","mp1jphdb") or die("Error " . mysqli_error($link));
 
-/* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-/*
-$delete_table = 'DELETE TABLE userinfo';
-$del_tbl = $link->query($delete_table);
-if ($delete_table) {
-        echo "Table userinfo has been deleted";
-}
-else {
-        echo "error!!";
 
-}
-*/
-$create_table = 'CREATE TABLE IF NOT EXISTS items  
+$create_table = 'CREATE TABLE PERSON
 (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(200) NOT NULL,
-    phone VARCHAR(20) NOT NULL,
-    filename VARCHAR(255) NOT NULL,
-    s3rawurl VARCHAR(255) NOT NULL,
-    s3finishedurl VARCHAR(255) NOT NULL,
-    state TINYINT(3) NOT NULL,
-    timestamp DATETIME NOT NULL,
-    PRIMARY KEY(id)
-)';
-
-
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    uname VARCHAR(20),
+    email VARCHAR(200),
+    phone VARCHAR(20),
+    filename VARCHAR(255),
+    s3rawurl VARCHAR(255),
+    s3finishedurl VARCHAR(255),
+    state TINYINT(3) CHECK(STATE IN(0,1,2)),
+    datetime TIMESTAMP)");
 
 $create_tbl = $link->query($create_table);
+
 if ($create_table) {
 	echo "Table is created or No error returned.";
 }
 else {
         echo "error!!";  
 }
+
 $link->close();
+
 ?>
